1fa486a96379237ca12a3dcac4bca733da93accb
darkdayzzz <darkday@iinet.net.au>
2016-12-27 01:30:53 +1100

    Lecture 63

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -11,7 +11,6 @@ UOpenDoor::UOpenDoor()
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 
@@ -20,15 +19,28 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	if (ActorThatOpens == nullptr) {
+		ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	}
+	AActor* Owner = GetOwner();
+	FRotator StartRotation = FRotator(0.0f, CloseAngle, 0.0f);
+	CloseAngle = Owner->GetTransform().GetRotation().Euler().Z;
+}
 
+
+void UOpenDoor::OpenDoor(bool bOpen)
+{
 	// find the owning actor
 	AActor* Owner = GetOwner();
 	int32 OldRotationDeg = Owner->GetTransform().GetRotation().Euler().Z;
-	// set the door rotation
-	FRotator NewRotation = FRotator(0.0f, 180.0f, 0.0f);
-	Owner->SetActorRotation(NewRotation);
-	int32 NewRotationDeg = Owner->GetTransform().GetRotation().Euler().Z;
-	UE_LOG(LogTemp, Warning, TEXT("Door was at %i degrees and now at %i degrees"), OldRotationDeg, NewRotationDeg);
+	if (((OldRotationDeg == CloseAngle) && bOpen) || ((OldRotationDeg != CloseAngle) && !bOpen)) {
+		// change the door rotation
+		FString ObjectName = GetOwner()->GetName();
+		FRotator NewRotation = FRotator(0.0f, bOpen ? CloseAngle + OpenAngleOffset : CloseAngle, 0.0f);
+		Owner->SetActorRotation(NewRotation);
+		int32 NewRotationDeg = Owner->GetTransform().GetRotation().Euler().Z;
+		UE_LOG(LogTemp, Warning, TEXT("Door was at %i degrees and now at %i degrees"), OldRotationDeg, NewRotationDeg);
+	}
 }
 
 
@@ -37,6 +49,18 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	// If ActorThatOpens is in the volume of PressurePlate then OpenDoor()
+	if (PressurePlate != nullptr)
+	{
+		if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+		{
+			OpenDoor(true);
+		}
+		else
+		{
+			OpenDoor(false);
+		}
+	}
+
 }
 
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -13,14 +13,26 @@ class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 
 public:	
 	// Sets default values for this component's properties
-	UOpenDoor();
+	UOpenDoor();	
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	void OpenDoor(bool bOpen);
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
-	
+private:
+	UPROPERTY(EditAnywhere)
+		int32 CloseAngle = 0; // angle of closed door
+
+	UPROPERTY(EditAnywhere)
+		int32 OpenAngleOffset = 90; // degrees to open door to from Closed door angle. ie. 90, -90, 45, -45, etc
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens;	
 };
